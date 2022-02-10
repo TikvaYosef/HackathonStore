da5477e0cc55b4cc4fac64631dd66b6988f4a94c
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-02-10 06:59:19 +0200

    add css

--- src/Components/Features/MedicinesCard/MedicinesCard.jsx
+++ src/Components/Features/MedicinesCard/MedicinesCard.jsx
@@ -1,20 +1,54 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import "../../Pages/Medicines/Medicines.css";
 
-
-export const MedicinesCard = ({ MedicineCareItem }) => {
+export function MedicinesCard({ MedicineCareItem }) {
     const {CompanyName,ProductName,price,ProductDescription,ProductImg,Quentity} = MedicineCareItem;
-
     return (
-        <div>
-            
-            <h1>Medicines </h1>
-            <p>{CompanyName}</p>
-            <p>{ProductName}</p>
-            <p>{price}</p>
-            <p>{ProductDescription}</p>
-            <img src={ProductImg} />
-            <p>{Quentity}</p>
-
-        </div>
-    )
+        <Card className='medicineCard' sx={{ maxWidth: 325, minWidth: 300 }}>
+            <CardActionArea className='medicineContent'>
+                <div className='opticPic'>
+                    <CardMedia
+                        component="img"
+                        height="150"
+                        width="150"
+                        image={ProductImg}
+                        alt="green iguana"
+                    />
+                </div>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {CompanyName}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    {ProductDescription}
+                        {/* {opticItem.price} */}
+                        {/* {opticItem.Quentity} */}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+        </Card>
+    );
 }
+
+// export const MedicinesCard = ({ MedicineCareItem }) => {
+//     const {CompanyName,ProductName,price,ProductDescription,ProductImg,Quentity} = MedicineCareItem;
+
+//     return (
+//         <div>
+//             {/* <p>{CompanyName}</p> */}
+//             {/* <p>{ProductName}</p> */}
+//             {/* <p>{price}</p> */}
+//             {/* <p>{ProductDescription}</p> */}
+//             {/* <img src={ProductImg} /> */}
+//             <p>{Quentity}</p>
+
+//         </div>
+//     )
+// }
--- src/Components/Pages/BodyCare/BodyCareCards.jsx
+++ src/Components/Pages/BodyCare/BodyCareCards.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import {  BodyCareContext } from "../../../Context/BodyCareContextProvider";
 import { BodyCareCard } from "../../Features/BodyCareCard/BodyCareCard";
+import './BodyCare.css';
 
 
 export const BodyCareCards = () => {
     const { BodyCare } = useContext(BodyCareContext);
 
     return (
-        <div>
+        <div className="bodyCareCardsContainer main">
             {
                 BodyCare.map((item) =>
                     <BodyCareCard BodyCareItem={item} />
