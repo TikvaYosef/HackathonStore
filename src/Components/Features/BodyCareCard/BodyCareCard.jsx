f5c23254bfc67d2708b5567b26ec585d36230b3d
TikvaYosef <tikvayosef13@gmail.com>
2022-02-10 04:52:35 +0200

    commit

--- src/Components/Features/BodyCareCard/BodyCareCard.jsx
+++ src/Components/Features/BodyCareCard/BodyCareCard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const BodyCareCard = ({ BodyCareItem }) => {
     return (
         <div>
-            <h1>Body Care</h1>
+          
             <p>{BodyCareItem.CompanyName}</p>
             <p>{BodyCareItem.ProductName}</p>
             <p>{BodyCareItem.price}</p>
